79648d16beae652d0c9170e99204b1d85bf34fbb
shanthosh004 <717822e151@kce.ac.in>
2025-06-25 15:06:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const button=document.getElementById("re");
+const col=document.getElementById("bl")
+
+function red()
+{
+    document.body.style.backgroundColor="red";
+}
+function black()
+{
+    document.body.style.backgroundColor="black";
+}
